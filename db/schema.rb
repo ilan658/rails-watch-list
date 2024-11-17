02df45f21ed27218295edc27d343f6850a9966f9
Ilan Attia <attia.ilan@gmail.com>
2024-11-17 10:57:58 +0100

    exo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_14_104746) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_15_113530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_14_104746) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["name"], name: "index_lists_on_name", unique: true
   end
 
